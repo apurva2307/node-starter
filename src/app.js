b73f4bde0d2770ff59fe6592925e9de6c70c4c59
apurva2307 <apurva.agra15@gmail.com>
2022-03-15 14:19:13 +0530

    cors removed

--- src/app.js
+++ src/app.js
@@ -9,16 +9,13 @@ const xssClean = require("xss-clean");
 const helmet = require("helmet");
 const rateLimiter = require("express-rate-limit");
 const mongoSanitize = require("express-mongo-sanitize");
-const cors = require("cors");
 
-app.set("trust proxy", 1);
 function setRateLimiter(options = { windowMs: 15 * 60 * 1000, max: 60 }) {
   app.use(rateLimiter(options));
 }
 app.use(helmet());
 app.use(xssClean());
 app.use(mongoSanitize());
-app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.json());
 
